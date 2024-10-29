9d78b82157ae3da1ed14ead0c0d96f84736ef132
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2024-10-29 23:08:37 +0100

    setting up developer environment with vscode

--- example/scenes/Airplane/visuals/Flaps/Flaps.gd
+++ example/scenes/Airplane/visuals/Flaps/Flaps.gd
@@ -8,9 +8,8 @@ var target_flap_position = 0.0
 
 
 func _on_Flaps_update_interface(values):
-	target_flap_position = deg_to_rad(values["flap"]*45.0)
+    target_flap_position = deg_to_rad(values["flap"] * 45.0)
 
 
 func _process(delta):
-	$FlapRotation.rotation.x = lerp($FlapRotation.rotation.x, target_flap_position, delta*5.0)
-
+    $FlapRotation.rotation.x = lerp($FlapRotation.rotation.x, target_flap_position, delta * 5.0)
